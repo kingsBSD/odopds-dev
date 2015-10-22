a95fde25672d99addfae983773aa738d2a8a9bfd
Giles Richard Greenway <augeas@gmail.com>
2015-10-22 17:40:26 +0100

    modified:   odoPdsReg/scripts/start_mongo-express.sh

--- odoPdsReg/scripts/start_mongo-express.sh
+++ odoPdsReg/scripts/start_mongo-express.sh
@@ -1,3 +1,6 @@
+until [ "$(pidof mongo)" ]; do
+    echo "Waiting for MongoDB"
+done
 #http://docs.mongodb.org/v2.4/tutorial/add-user-administrator/
 mongo admin --eval 'db.addUser( { user: "user", pwd: "pass", roles: [ "userAdminAnyDatabase" ] } )'
 cd /mongo-express
